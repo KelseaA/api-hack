c7dbb908f7577c90f14e50e8529df0cbce829523
Kelsea <andersonkelsea@gmail.com>
2016-04-04 19:06:49 -0400

    Added list styling and made responsive

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// document ready function
+$(function(){
+
+});
+
+// gets JSON
+function getRequest(){
+	var params = {
+
+	};
+	url = "https://api.seatgeek.com/2";
+	$.getJSON(url, params, function(data){
+
+	});
+}
+
